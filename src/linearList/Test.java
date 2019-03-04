bb647fd19d88de4b7a2c221bf2837f20c26e114a
ZhiningHe <747230196@qq.com>
2019-03-04 21:18:42 +0800

    线性表的基本操作：链表、顺序表

---
+++ src/linearList/Test.java
@@ -0,0 +1,13 @@
+package linearList;
+
+public class Test {
+    public static void main(String[] args) {
+        SequenceArrayimpl array = new SequenceArrayimpl();
+        array.set(0,1);
+        array.set(1,2);
+        array.add(3);
+        array.remove(1);
+        array.get(1);
+
+    }
+}
