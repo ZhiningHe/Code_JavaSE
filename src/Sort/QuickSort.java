0a2d7b604d89738a653775fe2e56fa6d88e4ffe6
ZhiningHe <747230196@qq.com>
2019-03-14 19:45:38 +0800

    1.快速排序 2.二路快排

--- src/Sort/QuickSort.java
+++ src/Sort/QuickSort.java
@@ -24,12 +24,10 @@ public class QuickSort {
         int j = start;
         //[j+1,i]是>key的区域
         int i = start+1;
-        while(i < end){
+        while(i <= end){
             //大于key不动 小于key跟array[j]交换
             if(array[i] <key){
-                int tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
+                swap(array,j+1,i);
                 j++;
             }
             i++;
@@ -40,6 +38,8 @@ public class QuickSort {
     }
 
 
+
+    //双路快速排序
     //优化重复元素多的数组
     public static int[] qiuckSort2(int[] array){
         if (array.length <=1) return array;
@@ -53,23 +53,24 @@ public class QuickSort {
         qiuck2(array,key+1,end);
     }
     private static int selectionKey2(int[] array, int start, int end){
-        int key = array[start];
-        //从头开始往后[start+1,i]
+        int random = (int)(Math.random()*(end-start+1)+start);
+        swap(array,random,start);
+        //找到一个随机key
+        int value = array[start];
+        //从头开始往后[start+1,i-1]
         int i = start+1;
-        //从尾开始往前[j,end]
-        int j = end-1;
+        //从尾开始往前[j+1,end]
+        int j = end;
         while (true){
                 //相当于把等于key的值均分到两边
-                while (array [i] <key && i<=end) i++;
-                while (array [j] >key && j>=start+1)  j--;
-                if (i>j) break;
-                //如果不是结束标志 就交换
-                swap(array,i,j);
+                while (i<=end && array [i] <value ) i++;
+                while (j>=start+1 &&array [j] >value ) j--;
                 //交换后两个指针都移动一步
-                i++;j--;
-
+            if(i>j) break;
+            swap(array,i,j);
+            i++;j--;
             }
-        swap(array,key,j);
+        swap(array,start,j);
             return j;
         }
 
