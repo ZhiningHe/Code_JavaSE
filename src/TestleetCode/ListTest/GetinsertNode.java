f1ed314d32898b4424b7f1af344c64c909c18fc7
ZhiningHe <747230196@qq.com>
2019-03-15 20:09:20 +0800

    寻找两个链表的交点

--- src/TestleetCode/ListTest/GetinsertNode.java
+++ src/TestleetCode/ListTest/GetinsertNode.java
@@ -14,7 +14,44 @@ public class GetinsertNode {
     public class Solution {
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
-            return null;
+            //如果有链表为null
+            if(headA ==null || headB ==null){
+                return null;
+            }
+            ListNode tmpA = headA;
+            ListNode tmpB = headB;
+            int lenA = 0,lenB = 0;
+            //判断是否相交
+            while (tmpA !=null){
+                tmpA = tmpA.next;
+                lenA++;
+            }
+            while (tmpB != null) {
+                tmpB = tmpB.next;
+                lenB++;
+            }
+            if(tmpA != tmpB){
+                //若是不相交
+               return null;
+            }
+             //从头开始定义指针
+                tmpA = headA;
+                tmpB = headB;
+            if(lenA <lenB){
+                for(int i=0;i<=lenB-lenA;i++){
+                    tmpB = tmpB.next;
+                }
+            }else {
+                for (int i=0;i<=lenA-lenB;i++){
+                    tmpA = tmpA.next;
+                }
+            }
+            //判断交点
+            while (tmpA != tmpB){
+                tmpA = tmpA.next;
+                tmpB = tmpB.next;
+            }
+            return tmpA;
         }
     }
 }
