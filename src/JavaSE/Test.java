464cb3504b520929c18c5ae7dd179fbdd9b092c7
ZhiningHe <747230196@qq.com>
2019-04-23 21:51:42 +0800

    线程部分

---
+++ src/JavaSE/Test.java
@@ -0,0 +1,51 @@
+package JavaSE;
+
+/**
+ * 对象回收自救
+ */
+public class Test{
+    //GCRoots
+    private static Test test;
+    public void Alive(){
+            System.out.println("i am alive");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("execute");
+        //test与当前对象建立联系
+        test = this;
+    }
+
+
+    public static void main(String[] args) throws Throwable {
+        //----------第一次--------------
+        //连接
+        test = new Test();
+        //断开连接
+        test = null;
+        //回收
+        System.gc();
+        Thread.sleep(500);
+        //判断是否还活着
+        if(test != null){
+            test.Alive();
+        }else{
+            System.out.println("i am dead");
+        }
+        //----------第二次-finalize已被调用过------
+        //自救会失败
+        test = null;
+        //回收
+        System.gc();
+        Thread.sleep(500);
+        //判断是否还活着
+        if(test != null){
+            test.Alive();
+        }else{
+            System.out.println("i am dead");
+        }
+    }
+}
+
