b1ef14f022b049114f18880ed9055e299f419a28
ZhiningHe <747230196@qq.com>
2019-05-03 22:49:06 +0800

    。

--- src/JavaSE/EmpApplication/Test.java
+++ src/JavaSE/EmpApplication/Test.java
@@ -2,6 +2,9 @@ package JavaSE.EmpApplication;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * 输入信息自动分隔存储
+ */
 //客户端主要实现
 public class Test {
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, NoSuchFieldException {
